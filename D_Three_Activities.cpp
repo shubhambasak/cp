db5a747c3fc0cade9fd39e3b12bcce38542d80d6
voz <138325452+shubhambasak@users.noreply.github.com>
2026-02-05 03:30:16 +0530

    Add files via upload

---
+++ D_Three_Activities.cpp
@@ -0,0 +1,92 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+
+vector<int> findMax3(vector<int> &arr) {
+	vector<pair<int, int>> tmp(arr.size());
+	for (int i = 0; i < tmp.size(); i++) { 
+		tmp[i].first = arr[i];
+		tmp[i].second = i;
+	}
+	sort(tmp.rbegin(), tmp.rend()); 
+	vector<int> ans(3);
+	for (int i = 0; i < 3; i++)
+		ans[i] = tmp[i].second;
+
+	return ans;
+}
+
+void solve(){
+	int n;
+	cin >> n; 
+    vector<int> a(n), b(n), c(n);
+    fr(i,n) cin >> a[i];
+    fr(i,n) cin >> b[i]; 
+    fr(i,n) cin >> c[i]; 
+
+	vector<int> maxa = findMax3(a); 
+	vector<int> maxb = findMax3(b);
+	vector<int> maxc = findMax3(c);
+
+	int ans = 0;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				int x = maxa[i], y = maxb[j], z = maxc[k];
+                
+				if ((x == y) or (y == z) or (z == x))
+					continue;
+				ans = max(ans, a[x] + b[y] + c[z]); 
+			}
+		}
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
