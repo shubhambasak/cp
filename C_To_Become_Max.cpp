3f542f71dc11423ea1953d98474fd47331041e7d
Shubham Basak <bloggershubham7011@gmail.com>
2026-05-26 22:49:48 +0530

    few more

---
+++ C_To_Become_Max.cpp
@@ -0,0 +1,116 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a)
+{
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a)
+{
+    for (auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a)
+{
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a)
+{
+    for (auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a)
+{
+    int n=a.size();
+
+    if (!n) return cout;
+
+    cout<<a[0];
+
+    for (int i=1; i<n; i++)
+        cout<<' '<<a[i];
+
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+int n,k;
+vi a;
+
+bool check(int x){
+    fr(i,n){
+        int cost=0;
+        for(int j=i,y=x;j<n;j++,y--){
+            if(y<=a[j])
+                break;
+
+            if(j==n-1) cost+=(int)1e9;
+            else cost+=(y-a[j]);
+        }
+        if(cost<=k) return true;
+    }
+    return false;
+}
+
+void solve(){
+    cin>>n>>k;
+    a.resize(n);
+    cin>>a;
+
+    int l=mxe(a);
+    int r=l+n;
+    int ans=l;
+
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(check(mid)) ans=mid, l=mid+1;
+        else r=mid-1;
+    }
+    cout<<ans;
+    nl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
