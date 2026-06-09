377445509c2fa41beb024e699b2ee6b006a0e9ae
Shubham Basak <bloggershubham7011@gmail.com>
2026-06-09 22:19:51 +0530

    few more

---
+++ C_Keshi_Is_Throwing_a_Party.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+int n;
+vector<pair<int,int>> v;
+
+bool check(int x){
+    int c=0;
+    fr(i,n) if(v[i].second>=c && v[i].first>=x-c-1) c++;
+    return c>=x;
+}
+
+void solve(){
+    cin>>n;
+    v.resize(n);
+    fr(i,n) cin>>v[i];
+    int l=1;
+    int r=n;
+    int best=1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(check(mid)) best=mid, l=mid+1;
+        
+        else r=mid-1;
+        
+    }
+    cout<<best<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
