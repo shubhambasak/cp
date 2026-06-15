128b2b51943da84fe301f5a68d666c021e0e7cee
Shubham Basak <bloggershubham7011@gmail.com>
2026-06-15 22:05:11 +0530

    few more

---
+++ A_Parsa_s_Humongous_Tree.cpp
@@ -0,0 +1,109 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+vector<pair<int,int>> bounds;
+vvi adj;
+vector<array<int,2>> dp;
+void dfs(int x,int p){
+    for(auto y:adj[x]){
+        if(y==p)
+            continue;
+        dfs(y,x);
+        dp[x][0]+=max(
+            dp[y][0]+abs(bounds[x].first-bounds[y].first),
+            dp[y][1]+abs(bounds[x].first-bounds[y].second)
+        );
+
+        dp[x][1]+=max(
+            dp[y][0]+abs(bounds[x].second-bounds[y].first),
+            dp[y][1]+abs(bounds[x].second-bounds[y].second)
+        );
+    }
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    bounds.resize(n);
+    fr(i,n) cin>>bounds[i];
+    adj.assign(n,{});
+    fr(i,n-1){
+        int u,v;
+        cin>>u>>v;
+        --u;
+        --v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dp.assign(n,{0,0});
+    dfs(0,-1);
+    cout<<max(dp[0][0],dp[0][1])<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
