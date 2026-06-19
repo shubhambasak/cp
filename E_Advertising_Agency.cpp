fa48accc05c289c540e54b35116a7bf5fa782ddd
Shubham Basak <bloggershubham7011@gmail.com>
2026-06-19 21:08:53 +0530

    few more

---
+++ E_Advertising_Agency.cpp
@@ -0,0 +1,151 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+
+using namespace std;
+
+int MOD=1000000007;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a)
+{
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a)
+{
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a)
+{
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a)
+{
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a)
+{
+    int n=a.size();
+
+    if(!n) return cout;
+
+    cout<<a[0];
+
+    for(int i=1;i<n;i++)
+        cout<<' '<<a[i];
+
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+int mul(int a,int b,int m=MOD)
+{
+    return ((a%m)*(b%m))%m;
+}
+
+int modexp(int a,int e,int m=MOD)
+{
+    a%=m;
+
+    int r=1;
+
+    while(e)
+    {
+        if(e&1)
+            r=mul(r,a,m);
+
+        a=mul(a,a,m);
+        e>>=1;
+    }
+
+    return r;
+}
+
+int inv(int a,int m=MOD)
+{
+    return modexp(a,m-2,m);
+}
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+
+    vi a(n);
+    cin>>a;
+
+    sort(a.rbegin(),a.rend());
+
+    int N=0;
+    int R=0;
+
+    fr(i,n)
+    {
+        if(a[i]==a[k-1])
+        {
+            N++;
+
+            if(i<k)
+                R++;
+        }
+    }
+
+    int val=1;
+
+    rep(i,1,R)
+    {
+        val=mul(val,N-i+1);
+        val=mul(val,inv(i));
+    }
+
+    cout<<val;
+    nl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T=1;
+    cin>>T;
+
+    while(T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
