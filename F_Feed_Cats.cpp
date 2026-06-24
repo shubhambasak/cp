79087ace9c8abfdf8fdea791098f890837add75a
Shubham Basak <bloggershubham7011@gmail.com>
2026-06-24 21:09:32 +0530

    few more

---
+++ F_Feed_Cats.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+
+using namespace std;
+
+int MOD=998244353;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>> a(m);
+    vi op(n+1,0);
+    vector<vi> del(n+1);
+    fr(i,m){
+        cin>>a[i];
+        op[a[i].first]++;
+        del[a[i].second].push_back(a[i].first);
+    }
+    multiset<int> cur;
+    vi dp(n+1,0);
+    rep(i,1,n){
+        dp[i]=dp[i-1];
+        fr(j,op[i]) cur.insert(i);
+        if(!cur.empty()){
+            int min_l=*cur.begin();
+            dp[i]=max(dp[i],dp[min_l-1]+sz(cur));
+        }
+        for(auto l:del[i]) cur.erase(cur.find(l));
+    }
+    cout<<dp[n]<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
