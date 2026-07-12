8c91a4eac3891c9d17dd1c52259fc20684d06ff0
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-12 21:12:59 +0530

    few more

--- C_Factorials_and_Powers_of_Two.cpp
+++ C_Factorials_and_Powers_of_Two.cpp
@@ -28,86 +28,71 @@ void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
 // ================================== take ip/op like vector,pairs directly!==================================
 
 template<typename typC,typename typD>
-istream &operator>>(istream &cin,pair<typC,typD> &a)
-{
+istream &operator>>(istream &cin,pair<typC,typD> &a){
     return cin>>a.first>>a.second;
 }
 
 template<typename typC>
-istream &operator>>(istream &cin,vector<typC> &a)
-{
+istream &operator>>(istream &cin,vector<typC> &a){
     for(auto &x:a) cin>>x;
     return cin;
 }
 
 template<typename typC,typename typD>
-ostream &operator<<(ostream &cout,const pair<typC,typD> &a)
-{
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
     return cout<<a.first<<' '<<a.second;
 }
 
 template<typename typC,typename typD>
-ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a)
-{
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
     for(auto &x:a) cout<<x<<'\n';
     return cout;
 }
 
 template<typename typC>
-ostream &operator<<(ostream &cout,const vector<typC> &a)
-{
+ostream &operator<<(ostream &cout,const vector<typC> &a){
     int n=a.size();
-
     if(!n) return cout;
-
     cout<<a[0];
-
-    for(int i=1;i<n;i++)
-        cout<<' '<<a[i];
-
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
     return cout;
 }
 
 // ===================================END Of the input module ==========================================
 
+int setbits(int n){
+    return __builtin_popcountll(n);
+}
+
 void solve(){
     int n;
     cin>>n;
-    vi deg(n,0);
-    fr(i,n-1){
-        int u,v;
-        cin>>u>>v;
-        u--,v--;
-        deg[u]++;
-        deg[v]++;
-    }
-    string s;
-    cin>>s;
-    int Z=0,O=0,Q=0,U=0;
-    rep(i,1,n-1){
-        if(deg[i]==1){
-            if(s[i]=='0') Z++;
-            else if(s[i]=='1') O++;
-            else Q++;
+    vi fact(15);
+    fact[0]=1;
+    rep(i,1,14) fact[i]=fact[i-1]*i;
+    vi v;
+    rep(i,3,14) v.push_back(fact[i]);
+    int ans=LLONG_MAX;
+    fr(mask,(1LL<<12)){
+        int sum=0,cnt=0;
+        fr(i,12){
+            if(mask&(1LL<<i)){
+                sum+=v[i];
+                cnt++;
+            }
         }
-        else if(s[i]=='?') U++;
+        if(sum>n)
+            continue;
+        cnt+=setbits(n-sum);
+        ans=min(ans,cnt);
     }
-
-    if(s[0]!='?'){
-        cout<<(s[0]=='1'?Z:O)+(Q+1)/2;
-        nl;
-    }
-    else{
-        if(Z==O && U&1) cout<<Z+(Q+1)/2;
-        else cout<<max(Z,O)+Q/2;
-        nl;
-    }
+    cout<<ans<<endl;
 }
 
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int T=1;
+    int T;
     cin>>T;
     while(T--) solve();
     return 0;
