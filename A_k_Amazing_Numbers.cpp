cf568a58a16155a73e374401ea237133d542e0c9
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-18 09:04:11 +0530

    few more

---
+++ A_k_Amazing_Numbers.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define mxe(v) *max_element(all(v))
+#define mne(v) *min_element(all(v))
+#define unq(v) v.resize(distance(v.begin(),unique(all(v))))
+#define bin(x,y) bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a,int b){a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a,int b){a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+void solve(){
+    int n;
+    cin>>n;
+    vi a(n);
+    cin>>a;
+    vvi pos(n+1);
+    fr(i,n) pos[a[i]].push_back(i);
+    vi ans(n,-1);
+    rep(i,1,n){
+        if(pos[i].empty())
+            continue;
+        int mx=pos[i][0]+1;
+        rep(j,1,sz(pos[i])-1) mx=max(mx,pos[i][j]-pos[i][j-1]);
+        mx=max(mx,n-pos[i].back());
+        for(int j=mx-1;j<n;j++){
+            if(ans[j]!=-1)
+                break;
+            ans[j]=i;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
+
