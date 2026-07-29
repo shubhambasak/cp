7a190bf3525d054c2dedcb388bee8d7210b2aa23
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-29 22:25:19 +0530

    few more

---
+++ C_Nested_Segments.cpp
@@ -0,0 +1,98 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define mxe(v) *max_element(all(v))
+#define mne(v) *min_element(all(v))
+#define unq(v) v.resize(distance(v.begin(),unique(all(v))))
+#define bin(x,y) bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a,int b){a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a,int b){a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<vector<int>> a(n);
+    fr(i,n){
+        int l,r;
+        cin>>l>>r;
+        a[i]={l,r,i+1};
+    }
+    sort(all(a),[](vector<int> &x,vector<int> &y){
+        if(x[0]==y[0]) return x[1]>y[1];
+        return x[0]<y[0];
+    });
+    int mx=a[0][1],ind=a[0][2];
+    rep(i,1,n-1){
+        if(mx>=a[i][1]){
+            cout<<a[i][2]<<" "<<ind<<endl;
+            return;
+        }
+        if(a[i][1]>mx){
+            mx=a[i][1];
+            ind=a[i][2];
+        }
+    }
+    cout<<"-1 -1"<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
+
