1370347464e4f59e20194ecff9242d4eadf4891b
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-30 21:58:37 +0530

    few more

---
+++ A_Balanced_Bitstring.cpp
@@ -0,0 +1,105 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define mxe(v) *max_element(all(v))
+#define mne(v) *min_element(all(v))
+#define unq(v) v.resize(distance(v.begin(),unique(all(v))))
+#define bin(x,y) bitset<y>(x)
+
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a,int b){a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a,int b){a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){
+    return cin>>a.first>>a.second;
+}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){
+    return cout<<a.first<<' '<<a.second;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+// ===================================END Of the input module ==========================================
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    rep(i,k,n-1){
+        if(s[i]=='1'){
+            if(s[i%k]=='0'){
+                cout<<"NO";
+                nl;
+                return;
+            }
+            s[i%k]='1';
+        }
+        else if(s[i]=='0'){
+            if(s[i%k]=='1'){
+                cout<<"NO";
+                nl;
+                return;
+            }
+            s[i%k]='0';
+        }
+    }
+    int c1=0,c0=0;
+    fr(i,k){
+        if(s[i]=='1') c1++;
+        else if(s[i]=='0') c0++;
+    }
+    if(c1>k/2 || c0>k/2) cout<<"NO";
+    else cout<<"YES";
+    nl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
