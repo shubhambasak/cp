b41115beb4063547ead9b3457a91caeb7076485e
Shubham Basak <bloggershubham7011@gmail.com>
2026-09-06 17:20:26 +0530

    few more

---
+++ C_Baby_Ehab_Partitions_Again.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i,n) for(int i=0;i<(n);i++)
+#define rep(i,a,n) for(int i=(a);i<=(n);i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define mxe(v) *max_element(all(v))
+#define mne(v) *min_element(all(v))
+#define unq(v) v.resize(distance(v.begin(),unique(all(v))))
+#define bin(x,y) bitset<y>(x)
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a,int b){a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a,int b){a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){return cin>>a.first>>a.second;}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){return cout<<a.first<<' '<<a.second;}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vi v(n);
+    cin>>v;
+    int sum=accumulate(all(v),0);
+    if(sum%2){
+        cout<<0;
+        return;
+    }
+    vb dp(sum/2+1,false);
+    dp[0]=true;
+    for(auto &x:v){
+        for(int i=sum/2;i>=x;i--){
+            dp[i]=dp[i]|dp[i-x];
+        }
+    }
+    if(!dp[sum/2]){
+        cout<<0;
+        return;
+    }
+    cout<<"1\n";
+    int div=1;
+    while(true){
+        fr(i,n){
+            if((v[i]/div)%2){
+                cout<<i+1;
+                return;
+            }
+        }
+        div*=2;
+    }
+    return;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
