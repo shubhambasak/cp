69d4956fd1c86da31ef4a7016fc5f82180adfb26
Shubham Basak <bloggershubham7011@gmail.com>
2026-09-25 07:03:40 +0530

    few more

---
+++ B_Irreducible_Anagrams.cpp
@@ -0,0 +1,82 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i,n) for(int i=0;i<(n);i++)
+#define rep(i,a,n) for(int i=(a);i<=(n);i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define mxe(v) *max_element(all(v))
+#define mne(v) *min_element(all(v))
+#define unq(v) v.resize(distance(v.begin(),unique(all(v))))
+#define bin(x,y) bitset<y>(x)
+using namespace std;
+
+int MOD=1e9+7;
+
+void modadd(int &a,int b){a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a,int b){a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a,int b){a=((a%MOD)*(b%MOD))%MOD;}
+
+template<typename typC,typename typD>
+istream &operator>>(istream &cin,pair<typC,typD> &a){return cin>>a.first>>a.second;}
+
+template<typename typC>
+istream &operator>>(istream &cin,vector<typC> &a){
+    for(auto &x:a) cin>>x;
+    return cin;
+}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const pair<typC,typD> &a){return cout<<a.first<<' '<<a.second;}
+
+template<typename typC,typename typD>
+ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a){
+    for(auto &x:a) cout<<x<<'\n';
+    return cout;
+}
+
+template<typename typC>
+ostream &operator<<(ostream &cout,const vector<typC> &a){
+    int n=a.size();
+    if(!n) return cout;
+    cout<<a[0];
+    for(int i=1;i<n;i++) cout<<' '<<a[i];
+    return cout;
+}
+
+void solve(){
+    string s;
+    cin>>s;
+    int n=s.size();
+    vvi sum(n+1,vi(26));
+    rep(i,1,n){
+        sum[i]=sum[i-1];
+        sum[i][s[i-1]-'a']++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        int cnt=0;
+        fr(i,26) cnt+=(sum[r][i]-sum[l-1][i]>0);
+        if(l==r || cnt>=3 || s[l-1]!=s[r-1]) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
